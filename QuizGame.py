fa0378928b733b3faea5fb10fb0c8ff2c54534ea
padartidayanabharathi <padartidayanabharathi@gmail.com>
2026-08-05 16:57:39 +0530

    Add files via upload

---
+++ QuizGame.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from questions import questions
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html", questions=questions)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    score = 0
+
+    for i, question in enumerate(questions):
+        user_answer = request.form.get(f"q{i}")
+
+        if user_answer == question["answer"]:
+            score += 1
+
+    return render_template("result.html", score=score, total=len(questions))
+
+if __name__ == "__main__":
+    app.run(debug=True)
